982798b5c1a68e5c197537e0528dd95fdc0e3488
Arya Suneesh <aryasuneesh3@gmail.com>
2026-04-16 23:24:34 +0530

    chore: add AppConfig for compile-time credentials via dart-define

---
+++ paperpulse_app/lib/core/config/app_config.dart
@@ -0,0 +1,6 @@
+class AppConfig {
+  AppConfig._();
+
+  static const String supabaseUrl = String.fromEnvironment('SUPABASE_URL');
+  static const String supabaseAnonKey = String.fromEnvironment('SUPABASE_ANON_KEY');
+}
